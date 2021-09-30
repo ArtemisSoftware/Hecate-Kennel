fdabf5e30af5b9938f03139c014f4641834b7a2b
gustavo <gustavo.isaac@gmail.com>
2021-09-30 09:22:35 +0100

    DogMapper

---
+++ data/src/main/java/com/artemissoftware/mappers/DogMapper.kt
@@ -0,0 +1,16 @@
+package com.artemissoftware.mappers
+
+import com.artemissoftware.domain.model.Dog
+import com.artemissoftware.remote.dto.DogDto
+
+fun DogDto.toDog(): Dog {
+
+    val breed = breeds[0]
+
+    return Dog(
+        name = breed.name,
+        breedGroup = breed.breedGroup,
+        temperament = breed.temperament,
+        imageUrl = url,
+    )
+}
