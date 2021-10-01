936031c9f1cc0cd35954440e9c54b3c6a75306ad
gustavo <gustavo.isaac@gmail.com>
2021-10-01 17:11:35 +0100

    NetworkModule

---
+++ app/src/main/java/com/artemissoftware/hecatekennel/di/NetworkModule.kt
@@ -0,0 +1,26 @@
+package com.artemissoftware.hecatekennel.di
+
+import com.artemissoftware.remote.DogApi
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object NetworkModule {
+
+    @Provides
+    @Singleton
+    fun provideDogApi(): DogApi {
+        return Retrofit.Builder()
+            .baseUrl("https://api.thedogapi.com/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create(DogApi::class.java)
+    }
+
+}
