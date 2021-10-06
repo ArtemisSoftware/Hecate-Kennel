7950016db87c3e40a286e1f730ba8d90db4fc09a
gustavo <gustavo.isaac@gmail.com>
2021-10-06 08:48:14 +0100

    Layout fragment_dogs

---
+++ buildSrc/src/main/java/Modules.kt
@@ -0,0 +1,9 @@
+object Modules {
+
+    const val app = ":app"
+    const val common = ":common"
+    const val data = ":data"
+    const val domain = ":domain"
+    const val presentation = ":presentation"
+
+}
--- presentation/src/main/java/com/artemissoftware/presentation/dogs/DogsFragment.kt
+++ presentation/src/main/java/com/artemissoftware/presentation/dogs/DogsFragment.kt
@@ -1,10 +1,11 @@
-package com.artemissoftware.presentation
+package com.artemissoftware.presentation.dogs
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.artemissoftware.presentation.R
 
 
 class DogsFragment : Fragment(R.layout.fragment_dogs) {
