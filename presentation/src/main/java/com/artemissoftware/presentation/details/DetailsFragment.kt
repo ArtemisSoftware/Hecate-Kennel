ab71ccb4ca5a26cf3f6715a8eb042af7cc881f60
gustavo <gustavo.isaac@gmail.com>
2021-10-06 12:14:08 +0100

    Layout fragment_details

---
+++ presentation/src/main/java/com/artemissoftware/presentation/details/DetailsFragment.kt
@@ -0,0 +1,17 @@
+package com.artemissoftware.presentation.details
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.artemissoftware.presentation.R
+
+
+class DetailsFragment : Fragment(R.layout.fragment_details) {
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+
+    }
+}
