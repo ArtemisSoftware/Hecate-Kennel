8f5f3bff97f57f2d6b5f75a6d97972b1bebc9e27
gustavo <gustavo.isaac@gmail.com>
2021-10-07 08:12:24 +0100

    Layout fragment_meme

---
+++ presentation/src/main/java/com/artemissoftware/presentation/meme/MemeFragment.kt
@@ -0,0 +1,16 @@
+package com.artemissoftware.presentation.meme
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.artemissoftware.presentation.R
+
+
+class MemeFragment : Fragment(R.layout.fragment_meme) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        //--Glide.with(context).load(GIF_URI).into(new GlideDrawableImageViewTarget(IMAGE_VIEW));
+    }
+}
