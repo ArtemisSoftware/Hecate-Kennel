9dbccf75a0a3f450cb5e052898c5192a5517277a
gustavo <gustavo.isaac@gmail.com>
2021-10-07 15:56:47 +0100

    Meme

--- data/src/main/java/com/artemissoftware/remote/DogApi.kt
+++ data/src/main/java/com/artemissoftware/remote/DogApi.kt
@@ -1,6 +1,7 @@
 package com.artemissoftware.remote
 
 import com.artemissoftware.remote.dto.DogDto
+import com.artemissoftware.remote.dto.MemeDto
 import retrofit2.http.GET
 import retrofit2.http.Query
 
@@ -10,4 +11,7 @@ interface DogApi {
     @GET("v1/images/search")
     suspend fun getDogs(@Query("limit") limit: String, @Query("order") order: String = "Desc"): List<DogDto>
 
+
+    @GET("v1/images/search")
+    suspend fun getMeme(@Query("mime_types") mimeType: String = "gif"): MemeDto
 }
---
+++ domain/src/main/java/com/artemissoftware/domain/model/Meme.kt
@@ -0,0 +1,9 @@
+package com.artemissoftware.domain.model
+
+data class Meme(
+
+    val url: String,
+    val width: Int,
+    val height: Int,
+
+)
