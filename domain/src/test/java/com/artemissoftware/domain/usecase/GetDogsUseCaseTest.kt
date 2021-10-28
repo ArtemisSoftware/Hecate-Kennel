860aa48c0d2532f637296a11ca44e50fdb98270f
gustavo <gustavo.isaac@gmail.com>
2021-10-28 21:26:26 +0100

    GetDogsUseCaseTest - part 1

--- domain/src/test/java/com/artemissoftware/domain/usecase/GetDogsUseCaseTest.kt
+++ domain/src/test/java/com/artemissoftware/domain/usecase/GetDogsUseCaseTest.kt
@@ -1,9 +1,76 @@
 package com.artemissoftware.domain.usecase
 
+import com.artemissoftware.common.Resource
+import com.artemissoftware.domain.ApiNetworkResponse
+import com.artemissoftware.domain.BaseUseCaseTest
+import com.artemissoftware.domain.model.Dog
+import com.artemissoftware.domain.repository.DogRepository
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.toList
+import kotlinx.coroutines.test.runBlockingTest
+import org.junit.Before
+import org.junit.Test
+import org.mockito.kotlin.mock
+import org.mockito.kotlin.whenever
+import org.mockito.kotlin.verify
+import org.mockito.kotlin.times
+import kotlin.test.assertEquals
 
-class GetDogsUseCaseTest {
+class GetDogsUseCaseTest : BaseUseCaseTest() {
+
+
+    private lateinit var dogRepository: DogRepository
+    private lateinit var getDogsUseCase: GetDogsUseCase
+
+
+    @Before
+    fun setUp() {
+        dogRepository = mock()
+        getDogsUseCase = GetDogsUseCase(repository = dogRepository)
+    }
+
+
+    @ExperimentalCoroutinesApi
+    @Test
+    fun `Should return Unit`() = runBlockingTest {
+
+        val dog = Dog(name = "Akita", breedGroup = "Working", temperament="Docile, Alert, Responsive, Dignified, Composed, Friendly, Receptive, Faithful, Courageous", imageUrl = "https://cdn2.thedogapi.com/images/S1_8kx5Nm_1280.jpg", lifeSpan= "10 - 14 years")
+        val list = listOf<Dog>(dog)
+
+        whenever(dogRepository.getDogs()).thenReturn(
+            ApiNetworkResponse(list)
+        )
+
+//        getDogsUseCase.invoke().apply {
+//
+//            val oo = it
+//
+//            assertEquals(this.javaClass, Flow<Resource<List<Dog>>>>::class.java)
+//        }
+
+        // Execute the use-case
+        val emissions: List<Resource<List<Dog>>> = getDogsUseCase.invoke().toList()
+        val lolo = (emissions[0] as Resource)
+        assert(lolo.data?.get(0)?.name  ==  dog.name)
+
+        //assertEquals(lolo.javaClass, Resource::class.java)
+
+
+//        whenever(authenticationRepository.validateEmail(nif = nif, email = email)).thenReturn(
+//            ApiNetworkResult(validateEmailEntity)
+//        )
+//
+//        validateEmailByEDPUseCase.invoke(nif = nif, email = email).apply {
+//            assertEquals(this.javaClass, ValidateEmailEntity::class.java)
+//        }
+//
+//        verify(authenticationRepository, times(1)).validateEmail(nif = nif, email = email)
+
+
+    }
+
 
 
 //    private lateinit var authenticationRepository: AuthenticationRepository
