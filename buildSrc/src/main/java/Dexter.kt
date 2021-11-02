0dc875ded41c19ff31924f107e85f8e62c1a4023
gustavo <gustavo.isaac@gmail.com>
2021-11-02 17:12:16 +0000

    Dexter dependencie

---
+++ buildSrc/src/main/java/Dexter.kt
@@ -0,0 +1,6 @@
+object Dexter {
+
+    private const val dexterVersion = "6.2.3"
+
+    const val dexter = "com.karumi:dexter:$dexterVersion"
+}
--- presentation/src/main/java/com/artemissoftware/presentation/pet/PetFragment.kt
+++ presentation/src/main/java/com/artemissoftware/presentation/pet/PetFragment.kt
@@ -1,5 +1,7 @@
 package com.artemissoftware.presentation.pet
 
+import android.Manifest
+import android.app.Activity.RESULT_OK
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -8,6 +10,28 @@ import android.view.ViewGroup
 import com.artemissoftware.presentation.R
 import com.artemissoftware.presentation.databinding.FragmentMemeBinding
 import com.artemissoftware.presentation.databinding.FragmentPetBinding
+import com.karumi.dexter.Dexter
+import com.karumi.dexter.PermissionToken
+
+import com.karumi.dexter.MultiplePermissionsReport
+import com.karumi.dexter.listener.PermissionRequest
+
+import com.karumi.dexter.listener.multi.MultiplePermissionsListener
+import androidx.core.app.ActivityCompat.startActivityForResult
+
+import android.content.Intent
+import android.provider.MediaStore
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.app.ActivityCompat.startActivityForResult
+
+
+
+
+
+
+
+
+
 
 
 class PetFragment : Fragment(R.layout.fragment_pet) {
@@ -25,7 +49,86 @@ class PetFragment : Fragment(R.layout.fragment_pet) {
 
         binding.imgPet.setOnClickListener {
 
+            Dexter.withContext(requireActivity())
+                .withPermissions(
+                    Manifest.permission.CAMERA,
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE
+                )
+                .withListener(object : MultiplePermissionsListener {
+                    override fun onPermissionsChecked(report: MultiplePermissionsReport) {
+                        if (report.areAllPermissionsGranted()) {
+                            //--showImagePickerOptions()
+                            launchGalleryIntent()
+                        }
+                        if (report.isAnyPermissionPermanentlyDenied) {
+                            //--showSettingsDialog()
+                        }
+                    }
+
+                    override fun onPermissionRationaleShouldBeShown(
+                        permissions: List<PermissionRequest>,
+                        token: PermissionToken
+                    ) {
+                        token.continuePermissionRequest()
+                    }
+                }).check()
+
         }
     }
+
+
+    private fun launchGalleryIntent() {
+//        val intent = Intent(this@MainActivity, ImagePickerActivity::class.java)
+//        intent.putExtra(
+//            ImagePickerActivity.INTENT_IMAGE_PICKER_OPTION,
+//            ImagePickerActivity.REQUEST_GALLERY_IMAGE
+//        )
+//
+//        // setting aspect ratio
+//        intent.putExtra(ImagePickerActivity.INTENT_LOCK_ASPECT_RATIO, true)
+//        intent.putExtra(ImagePickerActivity.INTENT_ASPECT_RATIO_X, 1) // 16x9, 1x1, 3:4, 3:2
+//        intent.putExtra(ImagePickerActivity.INTENT_ASPECT_RATIO_Y, 1)
+//        startActivityForResult(intent, REQUEST_IMAGE)
+
+        chooseImageFromGallery()
+    }
+
+
+    private fun chooseImageFromGallery() {
+        Dexter.withContext(requireActivity())
+            .withPermissions(Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE)
+            .withListener(object : MultiplePermissionsListener {
+                override fun onPermissionsChecked(report: MultiplePermissionsReport) {
+                    if (report.areAllPermissionsGranted()) {
+
+                        val pickPhoto = Intent(Intent.ACTION_PICK,MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+
+                        resultLauncher.launch(pickPhoto)
+                        //openSomeActivityForResult()
+                        //startActivityForResult(pickPhoto, REQUEST_GALLERY_IMAGE)
+                    }
+                }
+
+                override fun onPermissionRationaleShouldBeShown(
+                    permissions: List<PermissionRequest>,
+                    token: PermissionToken
+                ) {
+                    token.continuePermissionRequest()
+                }
+            }).check()
+    }
+
+
+    var resultLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+        if (result.resultCode == RESULT_OK) {
+            // There are no request codes
+            val data: Intent? = result.data
+            //--doSomeOperations()
+        }
+    }
+
+
+
+
 
 }
